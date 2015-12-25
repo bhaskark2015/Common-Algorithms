040c404dab4f4f9205d1826300ff1d7b0f8841b2
Bhaskar Kushwaha <bhaskar.kushwaha2009@gmail.com>
2015-12-25 16:37:19 +0530

    added clean () method in BigNum

--- BigNum/main.cpp
+++ BigNum/main.cpp
@@ -3,7 +3,7 @@
  * File:   main.cpp
  * Author: Bhaskar
  *
- * Created on December 24, 2015, 6:23 PM
+ * Created on December 24, 2015, 6:30 PM
  */
 
 #include <cstdlib>
@@ -15,14 +15,6 @@ using namespace std;
 class Bignum{
     string s;
     int chunk_size;
-public:
-    Bignum(string &s){
-        this->s=s;
-        chunk_size=getchunksize();
-    }
-    Bignum(){
-        chunk_size=getchunksize();
-    }
     int getchunksize(){
         int chunk_size=0;
         int max=INT_MAX;
@@ -32,15 +24,29 @@ public:
         }
         return chunk_size;
     }
+    int clean(){
+        int i,j;
+        for(i=0,j=0;i<s.length();i++){
+            if(s[i]<'0' || s[i]>'9')continue;
+            else s[j++]=s[i];
+        }
+        s.erase(j,s.length());
+    }
+    bool negative;
+public:
+    Bignum(string &s){
+        this->s=s;
+        chunk_size=getchunksize();
+        clean();
+        
+    }
+    Bignum():s("0"){
+        chunk_size=getchunksize();
+    }
     
-    friend Bignum operator+(Bignum&,Bignum &);    
-    friend ostream & operator<<( ostream &, const Bignum&); 
-    friend istream & operator>>(istream &,Bignum&);
-};
-
- Bignum operator+ ( Bignum &num1,Bignum &num2){
-        string &A=num1.s;
-        string &B=num2.s;
+ Bignum operator+ ( Bignum &num){
+        string &A=this->s;
+        string &B=num.s;
         
         int carry=0,i,j;
         string ans;
@@ -73,6 +79,9 @@ public:
         }
         return Bignum(ans);
     }
+    friend ostream & operator<<( ostream &, const Bignum&); 
+    friend istream & operator>>(istream &,Bignum&);
+};
 
 ostream& operator<<(ostream & output, const Bignum& num){
     output<< num.s;
@@ -81,17 +90,13 @@ ostream& operator<<(ostream & output, const Bignum& num){
 
 istream& operator>>(istream & input , Bignum & num ){
     input>>num.s ;
+    num.clean();
     return input;
 }
 
 
-/*
- * 
- */
 int main(int argc, char** argv) {
-//    Bignum A("641313654313543135513165435153135453135435135453515346541531341331341534");
-//    Bignum B("684313768431353435153135135153153453153134531531531354351351351351351351654513");
-//    
+  
 #define DEBUG
 #ifdef DEBUG
     freopen("input.txt","r",stdin);
