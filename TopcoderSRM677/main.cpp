aa8df225cc5319b373d3f5420615e88eefa99b03
Bhaskar Kushwaha <bhaskar.kushwaha2009@gmail.com>
2015-12-27 00:41:29 +0530

    topcoder from 11:00pm to 12:40 am IST 27th December 2015

---
+++ TopcoderSRM677/main.cpp
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Bhaskar
+ *
+ * Created on December 26, 2015, 10:07 PM
+ */
+
+#include <cstdlib>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class PalindromePrime{
+#define MAX_SIZE 1000
+    
+public:
+    
+    int count(int l,int r){
+    int primeseive[MAX_SIZE+1]={0};
+
+    for(int i=2;i<=MAX_SIZE;i++)
+        for(int k=2;k*i<=MAX_SIZE;k++){
+            primeseive[k*i]=1;
+        }
+    int palindromicCountUpto[MAX_SIZE+1]={0};
+    int count=0;
+    for(int i=2;i<=MAX_SIZE;i++)
+    {
+        if(primeseive[i]==0){
+            int temp=i;
+            int rev=0;
+            while(temp>0){rev=rev*10+(temp%10);temp/=10;}
+            if(rev==i) count++;
+        }
+        palindromicCountUpto[i]=count;
+    }
+    
+    return palindromicCountUpto[r]-palindromicCountUpto[l-1]; 
+}
+};
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    PalindromePrime test;
+    
+#define DEBUG
+#ifdef DEBUG
+    freopen("input.txt","r",stdin);
+#endif
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a>> b;
+        cout<<test.count(a,b)<<endl;
+    }
+    
+    
+    return 0;
+}
+
